03e96274561423a22f0589ef28e3df2fc2709949
Vic Wang <jerrywangvic@gmail.com>
2017-02-06 21:14:00 -0800

    clean code

--- app_eq.rb
+++ app_eq.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
-require_relative "./simplifier/methods/def_master_transform.rb"
-# require 'sinatra/reloader'
+require_relative "./simplifier/def_master_transform.rb"
 
 class C
   extend Transformation
@@ -11,39 +10,22 @@ get '/' do
 end
 
 post '/result' do
-  res = C.transform(params[:eq_input])
-  # erb " <br>La liste est #{res}", layout: :layout
-
-  # DEBUGGING BEGIN
-  p "in ruby console, let's look at res"
-  p res
-  p "+++++++++++++++"
-  # DEBUGGING END
-
-  return res
+  return C.transform(params[:eq_input])
 end
 
 post '/file_result' do
-
   begin
-
     input_path = params[:file_path].chomp.gsub(" ", "")
     output = File.new("./output.out", "w")
     input_file = File.open(input_path, "r").each do |line|
         output.puts C.transform(line.chomp)
     end
-
     input_file.close
     output.close
-    puts "----------"
-    notice = "Your results are ready in file `./output.out`. Thanks!"
-    puts notice
-    return notice
+    return "Your results are ready in file `./output.out`. Thanks!"
   rescue StandardError => e
-    2.times {puts ""}
-    puts e
-    puts "Please try again."
-    return failure_notice = "#{e}, please try again."
+    return "#{e}, please try again."
   end
-
+  # closing `begin-rescue`
 end
+# closing `post file_result`
